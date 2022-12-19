65492553923642cbe9ba198dc6dff5e36613957d
borisevtimov <boriss.evtimov@gmail.com>
2022-12-19 19:58:08 +0200

    Extended IdentityUser

--- UrbanLife.Data/Data/ApplicationDbContext.cs
+++ UrbanLife.Data/Data/ApplicationDbContext.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using UrbanLife.Data.Data.Models;
+#nullable disable warnings
 
 namespace UrbanLife.Data.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<User> Users { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ UrbanLife.Data/Data/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+#nullable disable warnings
+
+namespace UrbanLife.Data.Data.Models
+{
+    public class User : IdentityUser
+    {
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+
+        [MaxLength(20)]
+        public string LastName { get; set; }
+    }
+}
