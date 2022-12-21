f1c41a2b109c605121a10650d5e8a031d9ecc36f
borisevtimov <boriss.evtimov@gmail.com>
2022-12-22 00:14:20 +0200

    Completed layout and implemented drop-down partial account view

---
+++ UrbanLife.Web/wwwroot/js/profilePartialMenu.js
@@ -0,0 +1,14 @@
+﻿const aside = document.getElementById('profile-menu');
+
+document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('profile-picture') && aside.style.display == 'none') {
+        aside.style.display = 'block';
+    }
+    else if (event.target.tagName != 'ASIDE' && !event.target.classList.contains('profile-item')) {
+        aside.style.display = 'none';
+    }
+});
+
+document.addEventListener('load', function () {
+    aside.style.display = 'none';
+});
